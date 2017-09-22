69761857de857c2d0e8a1aa708def197afbe7ee1
JackieCMU <jqhetj@gmail.com>
2017-09-22 00:04:53 -0400

    test file for blink

---
+++ blink_testfile.py
@@ -0,0 +1,10 @@
+import unitest
+
+class TestStringMethods(unittest.TestCase):
+    def test_equal(self):
+        self.assertTrue(blink([1, 0, 0], 4) == [1, 1, 0])
+        self.assertTrue(blink([1, 0, 0, 0], 4) == [0, 0, 0, 0])
+        self.assertTrue(blink([1, 0, 0, 0], 24) == [0, 0, 0, 0])
+
+if __name__ == '__main__':
+    unittest.main()
