91eebabacfff1d46c9193d08c329aa013b3e471f
JackieCMU <jqhetj@gmail.com>
2017-09-24 02:09:22 -0400

    modeified testfile

--- crowded_cows_testfile.py
+++ crowded_cows_testfile.py
@@ -1,3 +1,5 @@
+from crowded_cows import crowded_cows
+
 import unittest
 
 
@@ -6,6 +8,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertTrue(crowded_cows([7, 3, 4, 2, 3, 4], 3) == 4)
         self.assertTrue(crowded_cows([7, 3, 4, 2, 3, 10, 4], 3) == 3)
         self.assertTrue(crowded_cows([7, 3, 1, 0, 4, 2, 16, 28, 3, 4], 3) == -1)
+        self.assertTrue(crowded_cows([2, 2, 1, 1, 2, 2, 3, 5, 3], 3) == 3)
 
 if __name__ == '__main__':
     unittest.main()
