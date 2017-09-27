a9e314550f9d2a389e4426796b1cc969993de67b
JackieCMU <jqhetj@gmail.com>
2017-09-26 20:52:07 -0400

    Update blink.py

--- blink.py
+++ blink.py
@@ -7,17 +7,17 @@ def blink(states, B):
         cur_state[pos] = state
     loop_state = []
 
-# the states are limited and repeated
-for i in range(B):
-    add_state = blink_all(cur_state)
-    # special case if all states are 0
-    if 1 not in add_state:
-        return add_state
-    elif add_state in loop_state:
-        break
-    loop_state.append(add_state)
+    # the states are limited and repeated
+    for i in range(B):
+        add_state = blink_all(cur_state)
+        # special case if all states are 0
+        if 1 not in add_state:
+            return add_state
+        elif add_state in loop_state:
+            break
+        loop_state.append(add_state)
 
-return loop_state[B % len(loop_state) - 1]
+    return loop_state[B % len(loop_state) - 1]
 
 # change state once based on left bulb's state
 def blink_once(left, cur):
